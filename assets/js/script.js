256807cfb86baefa2c11d34e50e397fe0f2469e6
Leonardo Sampaio <profleonardo95@gmail.com>
2022-11-04 17:31:12 -0300

    paginação de todas as telas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const quizzUserContainerElement = document.querySelector(".tela1-quizzUser .tela
 const quizzEveryElement = document.querySelector(".tela1-everyQuizz .tela1-containerQuizz")
 const noQuizzElemet = document.querySelector(".tela1-noQuizz")
 
+let idSelecionado;
 let quizzes = [];
 let quizzesStorage = [];
 let answer = {
@@ -16,6 +17,7 @@ let level = {
     text: "",
     inValue: 0
 }
+
 let quizzCreate = {
 	title: "",
 	image: "",
@@ -38,6 +40,12 @@ let quizzCreate = {
 	],
 	levels: []
 }
+let = quizzUni = {};
+
+getLocalStorageQuizzes();
+getQuizzes();
+//createQuizz();
+
 
 function getLocalStorageQuizzes(){
     let quizzesLocalStorage = localStorage.getItem("quizzes")
@@ -45,8 +53,6 @@ function getLocalStorageQuizzes(){
         quizzesStorage.push(...JSON.stringify(quizzesLocalStorage))
     }
 }
-
-getLocalStorageQuizzes()
 
 function getQuizzes(){
     let promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
@@ -63,6 +69,7 @@ function getUniqueQuizz(id){
     let promisse = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
     promisse.then((response) => {
         console.log(response.data)
+        quizzUni = response.data;
         quizzCreate = {
             title: "",
             image: "",
@@ -107,10 +114,11 @@ function handleQuizzes(){
     quizzEveryElement.innerHTML = ""
     if(quizzesStorage.length){
         quizzUserContainerElement.innerHTML = ""
-        noQuizzElemet.classList.add("hidden")
+        noQuizzElemet.classList.add("hidden")        
         quizzesStorage.forEach(quizz => {
             quizzUserContainerElement.innerHTML += `
-            <div class="tela1-quizz">
+            <div onclick="quizzTela2(this)" class="tela1-quizz">
+                <span class="hidden id-unitario">${quizz.id}</span>
                 <img src="${quizz.image}" />
                 <div class="tela1-gradientQuizz"></div>
                 <p>${quizz.title}</p>
@@ -125,7 +133,8 @@ function handleQuizzes(){
 
         quizzes.forEach(quizz => {
             quizzEveryElement.innerHTML += `
-            <div class="tela1-quizz">
+            <div onclick="quizzTela2(this)" class="tela1-quizz">
+                <span class="hidden id-unitario">${quizz.id}</span>
                 <img src="${quizz.image}" />
                 <div class="tela1-gradientQuizz"></div>
                 <p>${quizz.title}</p>
@@ -134,5 +143,100 @@ function handleQuizzes(){
     })
 }
 
-getQuizzes()
-//createQuizz()
+//---------------------PAGINAÇÃO-------------------
+
+function infoBasicoTela3(){		
+	
+	let telaCriar = document.querySelector('.tela1-noQuizz');
+    let telaInfoBasico = document.querySelector('.info-basico');
+    let quizzesUsuario = document.querySelector('.tela1-quizzUser');
+    let quizzesTodos = document.querySelector('.tela1-everyQuizz');
+    
+    telaCriar.classList.add('hidden');
+    telaInfoBasico.classList.remove('hidden');
+    quizzesUsuario.classList.add('hidden');
+    quizzesTodos.classList.add('hidden');    
+}
+
+function perguntasTela3(){		
+	
+    let telaInfoBasico = document.querySelector('.info-basico');
+    let perguntasQuiz = document.querySelector('.perguntas-quiz');
+    
+    telaInfoBasico.classList.add('hidden');
+    perguntasQuiz.classList.remove('hidden');    
+}
+
+function niveisTela3(){		
+    
+    let perguntasQuiz = document.querySelector('.perguntas-quiz');
+    let niveisQuiz = document.querySelector('.niveis-quiz');
+    
+    perguntasQuiz.classList.add('hidden');
+    niveisQuiz.classList.remove('hidden');    
+}
+
+function sucessoTela3(){    
+    
+    let niveisQuiz = document.querySelector('.niveis-quiz');
+    let sucessoQuiz = document.querySelector('.sucesso-quiz');    
+
+    niveisQuiz.classList.add('hidden'); 
+    sucessoQuiz.classList.remove('hidden');   
+}
+
+function voltarHome(){    
+    window.location.reload();
+}
+
+function quizzTela2(quizz){
+
+    
+    
+    idSelecionado = document.querySelector('.id-unitario');
+    idSelecionado = idSelecionado.innerHTML;
+
+    let telaQuizz = document.querySelector('.quizz-selecionado');
+    let tela1 = document.querySelector('.selecao-tela1'); 
+    let tela3 = document.querySelector('.tela3'); 
+
+    telaQuizz.classList.remove('hidden'); 
+    tela1.classList.add('hidden'); 
+    tela3.classList.add('hidden');
+
+    let quizzUnitario = `
+        <div class="banner">       
+            <h2>${quizzUni.title}</h2>
+        </div>            
+        <div class="card-quizz">
+            <h3>${quizzUni.title}</h3>
+            <div class="respostas">
+                <div class="item certo-errado-desmarcado">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHmXq_UovhRqnM96mGBIlrvg80vAinWuq0Vw&usqp=CAU" />
+                    <p>Gatíneo</p>
+                </div>
+                <div class="certo-marcado item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHmXq_UovhRqnM96mGBIlrvg80vAinWuq0Vw&usqp=CAU" />
+                    <p class="certo-marcado">Gatíneo</p>
+                </div>
+                <div class="item errado-marcado">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHmXq_UovhRqnM96mGBIlrvg80vAinWuq0Vw&usqp=CAU" />
+                    <p>Gatíneo</p>
+                </div>
+                <div class="item certo-errado-desmarcado">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHmXq_UovhRqnM96mGBIlrvg80vAinWuq0Vw&usqp=CAU" />
+                    <p>Gatíneo</p>
+                </div>
+            </div>
+        </div>`
+        getUniqueQuizz(idSelecionado);
+        telaQuizz.innerHTML = quizzUnitario 
+
+        console.log(quizzUni)
+
+    
+}
+
+
+
+
